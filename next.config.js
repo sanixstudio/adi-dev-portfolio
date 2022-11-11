b9991ee65524ae578643d52d425080ef5ce1597e
Adnan Niaz <sanixstudio@gmail.com>
2022-11-10 23:59:55 -0800

    chor: added redirection config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,9 @@
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        deviceSizes: [640, 768, 1024, 1280, 1536],
-        loader: "custom",
-        path: "/",
+module.exports = {
+    async redirects() {
+        return [{
+            source: '/',
+            destination: '/homepage1',
+            permanent: true,
+        }, ]
     },
-};
-
-module.exports = withPlugins(
-    [
-        [
-            withPWA,
-            {
-                pwa: {
-                    disable: process.env.NODE_ENV === "development",
-                    dest: "public",
-                    runtimeCaching,
-                },
-            },
-        ],
-    ],
-    nextConfig
-);
+}
