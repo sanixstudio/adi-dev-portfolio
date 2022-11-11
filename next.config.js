5f1642132cec06d7bb05b420feb8867e04a7701a
Adnan Niaz <sanixstudio@gmail.com>
2022-11-11 05:21:17 -0800

    chore: add minor changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,37 @@
-module.exports = {
-    async redirects() {
-        return [{
-            source: '/',
-            destination: '/homepage1',
-            permanent: true,
-        }, ]
+// module.exports = {
+//     async redirects() {
+//         return [{
+//             source: '/',
+//             destination: '/homepage1',
+//             permanent: true,
+//         }, ]
+//     },
+// }
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        deviceSizes: [640, 768, 1024, 1280, 1536],
+        loader: "custom",
+        path: "/",
     },
-}
+};
+
+module.exports = withPlugins(
+    [
+        [
+            withPWA,
+            {
+                pwa: {
+                    disable: process.env.NODE_ENV === "development",
+                    dest: "public",
+                    runtimeCaching,
+                },
+            },
+        ],
+    ],
+    nextConfig
+);
