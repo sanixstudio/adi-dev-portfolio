ba9ca38caeb5deb9c20687b5c0b5e9f718a1e359
Adnan Niaz <sanixstudio@gmail.com>
2022-11-11 20:41:21 -0800

    chore: minor changes

--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
-// module.exports = {
-//     async redirects() {
-//         return [{
-//             source: '/',
-//             destination: '/homepage1',
-//             permanent: true,
-//         }, ]
-//     },
-// }
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        deviceSizes: [640, 768, 1024, 1280, 1536],
-        loader: "custom",
-        path: "/",
+module.exports = {
+    async redirects() {
+        return [{
+            source: '/',
+            destination: '/homepage1',
+            permanent: true,
+        }, ]
     },
-};
+}
+// const withPlugins = require("next-compose-plugins");
+// const withPWA = require("next-pwa");
+// const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPlugins(
-    [
-        [
-            withPWA,
-            {
-                pwa: {
-                    disable: process.env.NODE_ENV === "development",
-                    dest: "public",
-                    runtimeCaching,
-                },
-            },
-        ],
-    ],
-    nextConfig
-);
+// const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//         deviceSizes: [640, 768, 1024, 1280, 1536],
+//         loader: "custom",
+//         path: "/",
+//     },
+// };
+
+// module.exports = withPlugins(
+//     [
+//         [
+//             withPWA,
+//             {
+//                 pwa: {
+//                     disable: process.env.NODE_ENV === "development",
+//                     dest: "public",
+//                     runtimeCaching,
+//                 },
+//             },
+//         ],
+//     ],
+//     nextConfig
+// );
---
+++ src/pages/homepage1.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { HeroSection } from "../components/containers";
+import { Layout2 } from "../components/layout";
+
+const Homepage3 = () => {
+  return (
+    <Layout2>
+      <Head>
+        <title> Adnan Niaz - Personal Portfolio</title>
+      </Head>
+      { /* Start Hero Section */}
+      <HeroSection scroll={false} />
+      { /* End Hero Section */}
+    </Layout2>
+  );
+};
+
+export default Homepage3;
